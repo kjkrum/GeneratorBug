7f081102fb36ca5c62e3dbbac8c7ffef1dc6afd4
Kevin <codeconcarne@protonmail.com>
2024-04-29 20:56:27 -0700

    Works as expected.

--- Generator/Generator.cs
+++ Generator/Generator.cs
@@ -3,7 +3,6 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Text;
 using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
 
@@ -18,56 +17,68 @@ namespace Generator
 		{
 			//System.Diagnostics.Debugger.Launch();
 			var valueProvider =
-				initContext.SyntaxProvider.ForAttributeWithMetadataName(
-					TriggerAttribute,
-					(node, _) => node is ClassDeclarationSyntax,
+				initContext.SyntaxProvider.CreateSyntaxProvider(
+					(node, _) => node is AttributeSyntax, // TODO pre-check name
 					Transform)
-				.WithComparer(TransformedComparer.Instance)
-				.Collect();
+				.Where(o => o != default)
+				.WithComparer(GeneratorInputComparer.Instance);
+				//.Collect();
 			initContext.RegisterSourceOutput(valueProvider, Generate);
 		}
 
-		private void Generate(SourceProductionContext sourceContext, ImmutableArray<Transformed> inputs)
+		private static GeneratorInput Transform(GeneratorSyntaxContext syntaxContext, CancellationToken cancel)
 		{
-			foreach(var input in inputs)
+			var symbolInfo = syntaxContext.SemanticModel.GetSymbolInfo(syntaxContext.Node, cancel);
+			// because there is still no official way to get a qualified name from an INamedTypeSymbol!
+			var qualifiedName = symbolInfo.Symbol.ContainingType.ToString();
+			if (qualifiedName != TriggerAttribute) return default;
+			var classNode = syntaxContext.Node.FirstAncestorOrSelf<ClassDeclarationSyntax>();
+			var classSymbol = syntaxContext.SemanticModel.GetDeclaredSymbol(classNode, cancel) as INamedTypeSymbol;
+			var input = new GeneratorInput()
 			{
-				sourceContext.AddSource($"{input.Name}.g.cs", $"// Generated at {DateTime.Now}. [Changed: {input.Changed}]");
-			}
+				Name = classSymbol.Name,
+				Sources = classSymbol.DeclaringSyntaxReferences.Select(o => o.GetSyntax().GetText())
+			};
+			return input;
 		}
 
-		private Transformed Transform(GeneratorAttributeSyntaxContext syntaxContext, CancellationToken _)
+		private void Generate(SourceProductionContext sourceContext, GeneratorInput input)
 		{
-			var symbol = syntaxContext.TargetSymbol as INamedTypeSymbol;
-			var texts = symbol.DeclaringSyntaxReferences.Select(o => o.GetSyntax().GetText());
-			return new Transformed() { Name = symbol.Name, SourceTexts = texts };
+			//foreach (var input in inputs)
+			//{
+				sourceContext.AddSource($"{input.Name}.g.cs", $"// Generated at {DateTime.Now}. [Changed={input.Changed}]");
+			//}
 		}
 
-		private class Transformed
+		private class GeneratorInput
 		{
 			internal string Name { get; set; }
-			internal IEnumerable<SourceText> SourceTexts { get; set; }
+			internal IEnumerable<SourceText> Sources { get; set; }
 			internal bool Changed { get; set; }
 		}
 
-		private class TransformedComparer : IEqualityComparer<Transformed>
+		private class GeneratorInputComparer : IEqualityComparer<GeneratorInput>
 		{
-			internal static readonly TransformedComparer Instance = new();
+			internal static readonly GeneratorInputComparer Instance = new();
 
-			public bool Equals(Transformed x, Transformed y)
+			public bool Equals(GeneratorInput x, GeneratorInput y)
 			{
-				var equal = Enumerable.SequenceEqual(x.SourceTexts, y.SourceTexts, SourceTextComparer.Instance);
+				if (x == null || y == null) return false;
+				var equal = Enumerable.SequenceEqual(x.Sources, y.Sources, SourceTextComparer.Instance);
 				y.Changed = !equal;
 				return equal;
-			}				
+			}
+				 
 
-			public int GetHashCode(Transformed obj) => 1;
+			public int GetHashCode(GeneratorInput obj) => 1;
 		}
 
 		private class SourceTextComparer : IEqualityComparer<SourceText>
 		{
 			internal static readonly SourceTextComparer Instance = new();
 
-			public bool Equals(SourceText x, SourceText y) => x.ContentEquals(y);
+			public bool Equals(SourceText x, SourceText y) =>
+				x != null & y != null && x.ContentEquals(y);
 
 			public int GetHashCode(SourceText obj) => 1;
 		}
